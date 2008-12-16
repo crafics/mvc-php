708b5f25b8c65e6bc04091f3ee7fe27115df1573
Manfred Weber <manfred.weber@gmail.com>
2008-12-16 17:05:44 +0000

    remove tabs

--- blog/urls.php
+++ blog/urls.php
@@ -3,20 +3,20 @@
 $urls = array(
 
 	/* blog stuff */
-	'^/blog/(?P<id>\d+)/'    	=> array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'singlePostAction'),
-	'^/blog/feed/$'				=> array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'feedAction'),
-	'^/blog/search/$'			=> array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'searchAction'),
-	'^/blog/tag/'               => array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'tagAction'),
-	'^/blog/posts/latest/'    	=> array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'latestBlogPostsAction'),
+	'^/blog/(?P<id>\d+)/' => array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'singlePostAction'),
+	'^/blog/feed/$' => array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'feedAction'),
+	'^/blog/search/$' => array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'searchAction'),
+	'^/blog/tag/' => array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'tagAction'),
+	'^/blog/posts/latest/' => array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'latestBlogPostsAction'),
 
 	/* admin stuff */
-	'^/blog/admin/login/'   		=> array('right'=>RIGHT_PUBLIC,'controller'=>'adminController','action'=>'loginAction'),
-	'^/blog/admin/post/$'          	=> array('right'=>RIGHT_ADMIN,'controller'=>'adminController','action'=>'postAction'),
-	'^/blog/admin/post/thankyou/$'	=> array('right'=>RIGHT_ADMIN,'controller'=>'adminController','action'=>'thankyouAction'),
-	'^/blog/admin/settings/$'   	=> array('right'=>RIGHT_ADMIN,'controller'=>'adminController','action'=>'settingsAction'),
-	'^/blog/admin/$'   				=> array('right'=>RIGHT_ADMIN,'controller'=>'adminController','action'=>'indexAction'),
+	'^/blog/admin/login/' => array('right'=>RIGHT_PUBLIC,'controller'=>'adminController','action'=>'loginAction'),
+	'^/blog/admin/post/$' => array('right'=>RIGHT_ADMIN,'controller'=>'adminController','action'=>'postAction'),
+	'^/blog/admin/post/thankyou/$' => array('right'=>RIGHT_ADMIN,'controller'=>'adminController','action'=>'thankyouAction'),
+	'^/blog/admin/settings/$' => array('right'=>RIGHT_ADMIN,'controller'=>'adminController','action'=>'settingsAction'),
+	'^/blog/admin/$' => array('right'=>RIGHT_ADMIN,'controller'=>'adminController','action'=>'indexAction'),
 
-	'^/blog/'          			=> array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'indexAction'),
-	'^/$'   					=> array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'indexAction'),
+	'^/blog/' => array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'indexAction'),
+	'^/$' => array('right'=>RIGHT_PUBLIC,'controller'=>'blogController','action'=>'indexAction'),
 
 );
