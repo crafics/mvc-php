c7274e30539287cfbb509c080d268d898416c835
Manfred Weber <manfred.weber@gmail.com>
2008-12-16 17:15:40 +0000

    docs

--- blog/config.php
+++ blog/config.php
@@ -1,35 +1,39 @@
 <?php
+/* The config file!
+*
+* @author	Manfred Weber
+* @date		20/11/08
+* @see		http://manfred.dschini.org/
+*/
+
 ini_set('display_errors',true);
 ini_set('error_reporting', E_ALL);
 ini_set('log_errors',1);
 
-/* database settings */
+/* defines here */
 define('DATABASE_NAME', 'blog');
 define('DATABASE_USER', 'dschini');
 define('DATABASE_PASS', 'dschini');
 define('DATABASE_HOST', 'localhost');
 define('DATABASE_PORT', 3306);
-
-/* define basedir */
 define('BASEDIR' ,dirname($_SERVER['DOCUMENT_ROOT']).DIRECTORY_SEPARATOR);
-
-/* defines */
 define("THEME_DEFAULT" ,BASEDIR."themes/default/");
-
-/* simple rights defs */
 define("RIGHT_PUBLIC"	,0);
 define("RIGHT_ADMIN"	,1);
 
-/* load proxies */
+/* PHP Libraries
+ * 
+ * Add you own libraries!!!
+ */
+
+/* Proxies */
 include(BASEDIR."lib/org/dschini/proxies/BlogpostProxy.php");
 include(BASEDIR."lib/org/dschini/proxies/BlogsettingsProxy.php");
 include(BASEDIR."lib/org/dschini/proxies/BlogcommentProxy.php");
-
-/* load controllers */
+/* Controllers */
 include(BASEDIR."lib/org/dschini/controllers/blogController.php");
 include(BASEDIR."lib/org/dschini/controllers/adminController.php");
-
-/* load helpers */
+/* Helpers */
 include(BASEDIR."lib/org/dschini/helpers/InputFilterHelper.php");
 include(BASEDIR."lib/org/dschini/helpers/RequestHelper.php");
 include(BASEDIR."lib/org/dschini/helpers/MySQLDriverHelper.php");
@@ -37,9 +41,11 @@ include(BASEDIR."lib/org/dschini/helpers/TemplateHelper.php");
 include(BASEDIR."lib/org/dschini/helpers/DateFormatHelper.php");
 include(BASEDIR."lib/org/dschini/helpers/URLHelper.php");
 
+
 /* load urls */
 include(BASEDIR."urls.php");
 
+/* start session */
 session_start();
 
 /*
